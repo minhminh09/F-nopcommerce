b85b4814d44a232f9fb86cb1eea2961be814cae8
minhminh09 <buingocminh09@gmail.com>
2024-08-09 11:47:07 +0700

    creart new project

---
+++ testcases/nopcommerce/account/Account_01_Register.java
@@ -0,0 +1,36 @@
+package nopcommerce.account;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Account_01_Register {
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void Register_01_Empty_Data() {
+	}
+
+	@Test
+	public void Register_02_Invalid_Email() {
+	}
+
+	@Test
+	public void Register_03_Invalid_Password() {
+	}
+
+	@Test
+	public void Register_04_Incorrect_confirm_password() {
+	}
+
+	@Test
+	public void Register_05_Succesfull() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
